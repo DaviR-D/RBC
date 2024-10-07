f6147433a26064a20c3f84626935b9ea061ae0de
Davi-RD <davirdavanzo@gmail.com>
2024-10-07 07:49:05 -0300

    Preparação do ambiente

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,12 +10,24 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/consulta',
+      name: 'Consultar',
+      component: HomeView
+    },
+    {
+      path: '/cadastro',
+      name: 'Cadastrar',
+      component: HomeView
+    },
+    {
+      path: '/revisao',
+      name: 'Revisar',
+      component: HomeView
+    },
+    {
+      path: '/gerencia',
+      name: 'Gerenciar',
+      component: HomeView
     }
   ]
 })
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <div class="button-grid">
+      <button>Consultar Casos Anteriores</button>
+      <button>Cadastrar Novo Caso</button>
+      <button>Revisar Casos</button>
+      <button>Gerenciar Base de Casos</button>
+    </div>
+  </div>
 </template>
+
+<script setup></script>
+<style scoped>
+.container {
+  position: relative;
+}
+.button-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 16px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+button {
+  padding: 10px;
+  font-size: 20px;
+  cursor: pointer;
+}
+</style>
