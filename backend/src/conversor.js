55c6614b3b6a44c3aa6e682faf06121b9c48f3dd
Davi-RD <davirdavanzo@gmail.com>
2024-10-14 09:44:30 -0300

    Desenvolvimento

--- backend/src/conversor.js
+++ backend/src/conversor.js
@@ -4,11 +4,11 @@ casos = require("./casos.json");
 for (let index = 0; index < casos.length; index++) {
   Object.keys(similaridade).forEach((characteristc) => {
     valor_string = casos[index][characteristc];
-    console.log(
-      similaridade[characteristc]["Variaveis"][valor_string]
-      //casos[0][characteristc]
-    );
+    if (valor_string == "desconhecido") return;
+    valor_peso = similaridade[characteristc]["variaveis"][valor_string]; //["valor"];
+    if (valor_peso == undefined) {
+      console.log(valor_string);
+      //console.log(similaridade[characteristc]);
+    }
   });
 }
-
-//console.log(casos[0]);
